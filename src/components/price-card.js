a1d50da63da00b05ef47d816fafa22d19e9ec86d
Mohammad Taha <taha2000mohammad@gmail.com>
2022-07-04 17:17:17 +0500

    Pricing Section

--- src/components/price-card.js
+++ src/components/price-card.js
@@ -1,6 +1,7 @@
 import { Box, Card, Text, Heading, Button } from 'theme-ui';
 import React from 'react';
 import List from './list';
+import ButtonGroup from './button-group';
 
 export default function PriceCard({
   data: {
@@ -14,7 +15,40 @@ export default function PriceCard({
   },
 }) {
   return (
-    <h1>PriceCard</h1>
+    <Card
+      className={header ? 'package__card active' : 'package__card'}
+      sx={styles.pricingBox}
+    >
+      {header && <Text sx={styles.header}>{header}</Text>}
+      <Box>
+        <Box className="package__header" sx={styles.pricingHeader}>
+          <Heading className="package__name" variant="title">
+            {name}
+          </Heading>
+          <Text as="p">{description}</Text>
+        </Box>
+        <List items={points} childStyle={styles.listItem} />
+        <Text className="package__price" sx={styles.price}>
+          {priceWithUnit} <span>/ Monthly</span>
+        </Text>
+        <Box sx={styles.buttonGroup}>
+          <Button variant="primary" aria-lable={buttonText}>
+            {buttonText}
+          </Button>
+          {anotherOption && (
+            <Button
+              variant="textButton"
+              className="free__trial"
+              aria-label={anotherOption}
+              sx={{ color: 'black' }}
+            >
+              {anotherOption}
+            </Button>
+          )}
+        </Box>
+      </Box>
+    </Card>
+    // <h1>PriceCard</h1>
   );
 }
 
